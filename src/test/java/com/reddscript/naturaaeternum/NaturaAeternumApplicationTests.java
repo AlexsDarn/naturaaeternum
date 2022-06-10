f9e9c707712bbc2c204798c35dd84737099f91d3
AlexsDarn <alexis.salazars@uqvirtual.edu.co>
2022-06-10 17:04:51 -0500

    Se creó la aplicación

---
+++ src/test/java/com/reddscript/naturaaeternum/NaturaAeternumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddscript.naturaaeternum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturaAeternumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
