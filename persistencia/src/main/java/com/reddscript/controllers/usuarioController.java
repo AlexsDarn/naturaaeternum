83bbc682044599a40ea28e99d467e217fb37baa3
AlexsDarn <alexis.salazars@uqvirtual.edu.co>
2022-06-11 22:13:42 -0500

    Se agrego la función de agregar un usuario.

--- persistencia/src/main/java/com/reddscript/controllers/usuarioController.java
+++ persistencia/src/main/java/com/reddscript/controllers/usuarioController.java
@@ -3,10 +3,7 @@ package com.reddscript.controllers;
 import com.reddscript.dao.UsuarioDao;
 import com.reddscript.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +24,14 @@ public class usuarioController {
         return usuario;
     }
 
-    @RequestMapping(value="api/usuarios")
+    @RequestMapping(value="api/usuarios", method = RequestMethod.GET)
     public List<Usuario> getUsuarios(){
      return usuarioDao.getUsuarios();
+    }
+
+    @RequestMapping(value="api/usuarios", method = RequestMethod.POST)
+    public void registrarUsuarios(@RequestBody Usuario usuario){
+        usuarioDao.registrar(usuario);
     }
 
    @RequestMapping(value="usuario45")
---
+++ persistencia/src/main/resources/static/js/registrar.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+    //on ready
+});
+    console.log("culo");
+
+async function registrarUsuario(){
+console.log("aaa");
+let datos = {};
+    datos.nombre = document.getElementById('txtNombre').value;
+    datos.apellido =document.getElementById('txtApellido').value;
+    datos.email =document.getElementById('txtEmail').value;
+    datos.password =document.getElementById('txtPassword').value;
+
+    let repeatPassword =document.getElementById('txtRepeatPassword').value;
+
+    if(repeatPassword != datos.password){
+        alert('Contraseñas diferentes');
+        return;
+    }
+
+    const request = await fetch('api/usuarios', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+        body: JSON.stringify(datos)
+    });
+    const usuarios = await request.json();
+}
